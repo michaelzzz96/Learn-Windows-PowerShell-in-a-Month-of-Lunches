87fcafa3e121a9527706b7b006b04a61d1eb3c45
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-03 19:00:42 -0500

    Add files via upload

---
+++ Chapter_3/Chapter_3.ps1
@@ -0,0 +1,31 @@
+﻿Write-Output 'Question 1'
+Update-Help -Force
+
+Write-Output 'Question 2'
+Help *html*
+
+Write-Output 'Question 3'
+Get-Command -Noun file, Printer
+
+Write-Output 'Question 4'
+Help *Process*
+
+Write-Output 'Question 5'
+Get-Command -Verb write -Noun EventLog
+
+Write-Output 'Question 6'
+Get-Command -Noun alias
+
+Write-Output 'Question 7'
+Help *transcript*
+
+Write-Output 'Question 8'
+help Get-EventLog -Parameter Newest
+
+Write-Output 'Question 9'
+Help Get-Service -Parameter computername
+
+Write-Output 'Question 10'
+Help Get-Process -Parameter computername
+
+Write-Output 'Question 11'
