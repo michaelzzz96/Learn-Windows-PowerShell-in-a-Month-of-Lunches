5c5f33804b479a87cab84f09df491638efc534ec
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-04 22:40:31 -0500

    Add files via upload

---
+++ Chapter_4/Chapter_4.ps1
@@ -0,0 +1,26 @@
+﻿Write-Host 'Question 1'
+Get-Process
+
+Write-Host 'Question 2'
+Get-EventLog -Newest 100 -LogName Application
+
+Write-Host 'Question 3'
+Get-Command -CommandType Cmdlet
+
+Write-Host 'Question 4'
+Get-Alias
+
+Write-Host 'Question 5'
+New-Alias -Name note -Value Get-ChildItem
+
+Write-Host 'Question 6'
+Get-Service -Name M*
+
+Write-Host 'Question 7'
+Get-Help *firewall*
+Write-Host 'Using Get-Help to find the to discover the necessary cmdlet'
+Get-NetFirewallRule
+Write-Host 'Question 8'
+Get-Help Get-NetFirewallRule -Detailed
+Write-Host 'Or'
+Get-NetFirewallRule -Direction Inbound
