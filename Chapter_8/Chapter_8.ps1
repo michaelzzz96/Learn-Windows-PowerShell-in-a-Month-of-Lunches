e64d16124555af65365c8cf9d82ef70f71a95d50
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-08 21:39:08 -0500

    Add files via upload

---
+++ Chapter_8/Chapter_8.ps1
@@ -0,0 +1,17 @@
+Write-Host 'Question 1'
+Get-Random
+
+Write-Host 'Question 2'
+Get-DateWrite-Host 'Question 3'
+get-date | Get-MemberWrite-Host 'Question 4'
+get-date | Select-Object -Property DayOfWeek
+
+Write-Host 'Question 5'
+Get-HotfixWrite-Host 'Question 6'Get-HotFix | Sort InstalledOn | Select InstalledOn,InstalledBy,HotFixIDWrite-Host 'Question 7'Get-HotFix | Sort Description | Select
+Description,InstalledOn,InstalledBy,HotFixID | ConvertTo-Html -Title
+"HotFix Report" | Out-File HotFixReport.htm
+Write-Host 'Question 8'Get-EventLog -LogName System -Newest 50 | Sort TimeGenerated,Index | Select
+Index,TimeGenerated,Source | Out-File elogs.txt
+
+
+
