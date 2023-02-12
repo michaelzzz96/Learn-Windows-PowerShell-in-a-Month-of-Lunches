03a40a39f463b8eaded9649b7dbc61c3faf70cbc
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-11 23:00:37 -0500

    Add files via upload

---
+++ Chapter_11/Chapter_11.ps1
@@ -0,0 +1,10 @@
+﻿import-module NetAdapter
+get-netadapter -physical
+
+Import-Module DnsClient
+Get-DnsClientCache -type AAAA,A
+
+Get-Hotfix -Description 'Security Update'No. The object that Get-Service uses doesnt have that information. get-hotfix -Description Update | where {$_.InstalledBy -match
+"administrator"}
+
+get-process -name svchost,conhost
