f9328e36976f2d907ac6228f9c7b00ac778e4021
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-11 23:01:57 -0500

    Add files via upload

---
+++ Chapter_11/Chapter_11/Chapter_11.ps1
@@ -0,0 +1,14 @@
+﻿Write-Host 'Question 1'
+import-module NetAdapter
+get-netadapter -physical
+
+Write-Host 'Question 2'
+Import-Module DnsClient
+Get-DnsClientCache -type AAAA,A
+
+Write-Host 'Question 3'
+Get-Hotfix -Description 'Security Update'Write-Host 'Question 4'No. The object that Get-Service uses doesnt have that information. Write-Host 'Question 5'get-hotfix -Description Update | where {$_.InstalledBy -match
+"administrator"}
+
+Write-Host 'Question 6'
+get-process -name svchost,conhost
