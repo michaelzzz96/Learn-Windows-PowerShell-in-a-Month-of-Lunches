4361550471d1e70e949deaadcea679320fb7e0bc
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-12 20:59:32 -0500

    Add files via upload

---
+++ Chapter_12/Chapter_12.ps1
@@ -0,0 +1,12 @@
+﻿#hide the command output
+mkdir C:\Labs | out-null
+#import the module
+import-module SMBShare
+#this is a single line command
+$share=New-smbshare -name 'LABS' -Path 'C:\Labs' -Description 'PowerShell
+Labs' -ChangeAccess 'Everyone' -FullAccess 'Administrators' -CachingMode
+Documents
+#display the share object
+Write-Output $share
+#pipe the share object to Get-SmbShareAccess to display permissions
+$share | Get-SmbShareAccess
