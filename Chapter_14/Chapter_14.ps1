629738c1726d78042623a99770ddb970e55fe21e
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-14 14:53:29 -0500

    Add files via upload

---
+++ Chapter_14/Chapter_14.ps1
@@ -0,0 +1,6 @@
+﻿Write-Host 'Question 1'
+Get-CimClass win32_networkadapterconfiguration | select -expand methods |
+where Name -match "dhcp"Write-Host 'Question 2'get-ciminstance win32_operatingsystem | Select BuildNumber,Caption,
+@{l='Computername';e={$_.CSName}},
+@{l='BIOSSerialNumber';e={(get-ciminstance win32_bios).serialnumber }} | ft
+-autoWrite-Host 'Question 3'Get-WmiObject –class Win32_QuickFixEngineeringWrite-Host 'Question 4'get-wmiobject win32_service | Select Name,State,StartMode,StartNameWrite-Host 'Question 5'get-wmiobject -list *product
