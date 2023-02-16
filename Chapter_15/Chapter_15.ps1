e3fc3e667a48fe7df4ec356a46b1c2aa5d0cdb34
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-15 20:47:04 -0500

    Add files via upload

---
+++ Chapter_15/Chapter_15.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host 'Question 1'Start-Job {dir c:\ -recurse –filter '*.ps1'}Write-Host 'Question 2'Invoke-Command –scriptblock {dir c:\ -recurse –filter *.ps1} –computername
+(get-content computers.txt) -asjobWrite-Host 'Question 3'$Trigger=New-JobTrigger -At "6:00AM" -DaysOfWeek
+"Monday","Tuesday","Wednesday","Thursday","Friday" –Weekly
+$command={ Get-EventLog -LogName System -Newest 25 -EntryType Error |
+Export-Clixml c:\work\25SysErr.xml}
+Register-ScheduledJob -Name "Get 25 System Errors" -ScriptBlock $Command
+-Trigger $Trigger
+#check on what was created
+Get-ScheduledJob | Select *
+
+Write-Host 'Question 4'
+Receive-Job –id 1 -keep
