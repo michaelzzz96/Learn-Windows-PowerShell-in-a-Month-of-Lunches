f3f10f61abaa2e075cb05110fc1bf4475fa2b251
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-16 19:34:24 -0500

    Add files via upload

---
+++ Chapter_16/Chapter_16.ps1
@@ -0,0 +1,13 @@
+﻿Write-Host 'Question 1'
+get-service | Get-Member -MemberType MethodWrite-Host 'Question 2'get-process | Get-Member -MemberType MethodWrite-Host 'Question 3'Get-CimClass -ClassName Win32_Process | Select-Object -ExpandProperty CimClassMethods
+
+
+Write-Host 'Question 4'
+Get-Process -Name Notepad | Stop-Process
+Get-Process -Name Notepad | ForEach-Object {$_.kill()}
+Get-CimInstance -ClassName Win32_Process -filter "name = 'notepad.exe'" | Invoke-CimMethod -MethodName Terminate
+Stop-Process -Name Notepad
+
+
+Write-Host 'Question 5'
+Get-Content C:\dev\servers.txt | ForEach-Object {$_.toUpper()}
