ab7afdc8d3e77603faa17fdf46d802bcdc216c1c
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-18 21:04:30 -0500

    Add files via upload

---
+++ Chapter_18/Chapter_18.ps1
@@ -0,0 +1,6 @@
+﻿Write-Host 'Question 1'
+invoke-command {get-wmiobject win32_bios} -comp localhost,$env:computername
+–asjob
+
+Write-Host 'Question 2'
+$results=Receive-Job 4 –keepWrite-Host 'Question 3'$resultsWrite-Host 'Question 4'$results | export-clixml bios.xml
