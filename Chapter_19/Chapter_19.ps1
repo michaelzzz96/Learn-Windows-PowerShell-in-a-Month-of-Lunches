5857dbaeeaa6f455f4b002f0b70c79d9dfd9b4f5
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-19 21:13:39 -0500

    Add files via upload

---
+++ Chapter_19/Chapter_19.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host 'Question 1'
+write-output (100*10)Write-Host 'Question 2'$a=100*10
+Write-Host $a
+Write-Host "The value of 100*10 is $a"
+Write-Host (100*10)
+
+Write-Host 'Question 3'
+$name=Read-Host "Enter a name"
+Write-host $name -ForegroundColor Yellow
+
+Write-Host 'Question 4'
+Read-Host "Enter a name" | where {$_.length -gt 5}
