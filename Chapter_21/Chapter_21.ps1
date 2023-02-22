62d7cf7f75ab941a82af9ff147d473ab16cbfcde
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-21 20:21:23 -0500

    Add files via upload

---
+++ Chapter_21/Chapter_21.ps1
@@ -0,0 +1,4 @@
+﻿Write-Host 'Question 1'
+Get-WmiObject Win32_LogicalDisk -comp $env:computername -filter "drivetype=3" |
+Where { ($_.FreeSpace / $_.Size) -lt .1 } |
+Select -Property DeviceID,FreeSpace,Size
