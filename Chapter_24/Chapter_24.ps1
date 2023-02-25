8f62869c787b6b8766e30c95fcba4204ecfe26ec
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-24 21:22:16 -0500

    Add files via upload

---
+++ Chapter_24/Chapter_24.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host 'Question 1'
+dir c:\windows | where {$_.name -match "\d{2}"}
+
+Write-Host 'Question 2'
+Get-Process | where {$_.Company -match "microsoft"} | Select-Object -Property Id,Name,Company
+
+
+Write-Host 'Question 3'
+get-content .\WindowsUpdate.log | Select-string "Start[\w+\W+]+Agent:
+Installing Updates"
+
